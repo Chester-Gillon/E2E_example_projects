9582d6fffe019420db40c861ae3a121ae1accf39
Chester Gillon <chester.gillon@metronet.co.uk>
2017-10-30 15:25:48 +0000

    Add AM3352_GCC_class_variables

---
+++ AM3352_GCC_class_variables/master_class.cpp
@@ -0,0 +1,20 @@
+/*
+ * master_class.cpp
+ *
+ *  Created on: 5 Mar 2017
+ *      Author: Mr_Halfword
+ */
+
+#include "child_class.h"
+#include "master_class.h"
+
+MasterClass::MasterClass()
+{
+
+}
+
+MasterClass::~MasterClass()
+{
+
+}
+
