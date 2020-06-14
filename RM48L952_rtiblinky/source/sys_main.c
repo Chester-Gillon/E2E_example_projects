b1b5a5d6b3de038439a6620b42bf1b5ab36de93b
Chester Gillon <chester.gillon@metronet.co.uk>
2020-06-14 15:08:19 +0100

    Reduce RM48L952_rtiblink CPU clock from 220 MHz to 40 MHz. Still can't repeat the failure with a XDS110.

--- RM48L952_rtiblinky/source/sys_main.c
+++ RM48L952_rtiblinky/source/sys_main.c
@@ -1,5 +1,14 @@
+/** @file sys_main.c 
+*   @brief Application main file
+*   @date 11-Dec-2018
+*   @version 04.07.01
+*
+*   This file contains an empty main function,
+*   which can be used for the application.
+*/
+
 /* 
-* Copyright (C) 2009-2015 Texas Instruments Incorporated - www.ti.com
+* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
 * 
 * 
 *  Redistribution and use in source and binary forms, with or without 
@@ -23,14 +32,15 @@
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
 *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
 *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION HOWEVER CAUSED AND ON ANY
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-*  INCLUDING NEGLIGENCE OR OTHERWISE ARISING IN ANY WAY OUT OF THE USE
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 */
+
 
 /* USER CODE BEGIN (0) */
 /* USER CODE END */
@@ -38,7 +48,6 @@
 /* Include Files */
 
 #include "sys_common.h"
-#include "system.h"
 
 /* USER CODE BEGIN (1) */
 #include "rti.h"
@@ -46,10 +55,12 @@
 #include "gio.h"
 /* USER CODE END */
 
-
 /** @fn void main(void)
 *   @brief Application main function
+*   @note This function is empty by default.
 *
+*   This function is called after startup.
+*   The user can use this function to implement the application.
 */
 
 /* USER CODE BEGIN (2) */
@@ -61,8 +72,7 @@ int cv_index;
 
 /* USER CODE END */
 
-
-void main(void)
+int main(void)
 {
 /* USER CODE BEGIN (3) */
 
@@ -97,7 +107,10 @@ void main(void)
     }
 
 /* USER CODE END */
+
+    return 0;
 }
+
 
 /* USER CODE BEGIN (4) */
 /* Note-You need to remove rtiNotification from notification.c to avoid redefinition */
