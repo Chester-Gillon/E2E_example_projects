53ca18539def3eb074efebcc97018f1e424510c4
Chester Gillon <chester.gillon@metronet.co.uk>
2020-08-22 22:10:09 +0100

    Add read function which allows a MSP430 GNU program to support scanf

---
+++ MSP430F5529_GCC_scanf/read.c
@@ -0,0 +1,76 @@
+/* Copyright (c) 2013 Red Hat, Inc. All rights reserved.
+
+   This copyrighted material is made available to anyone wishing to use, modify,
+   copy, or redistribute it subject to the terms and conditions of the BSD
+   License.   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY expressed or implied, including the implied warranties
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  A copy of this license
+   is available at http://www.opensource.org/licenses. Any Red Hat trademarks that
+   are incorporated in the source code or documentation are not subject to the BSD
+   License and may only be used or replicated with the express permission of
+   Red Hat, Inc.
+ */
+
+#include <string.h>
+
+#include "cio.h"
+
+static int
+read_chunk (int fd, char *buf, int len)
+{
+    int out_length;
+
+    __CIOBUF__.length[0] = 0;
+    __CIOBUF__.length[1] = 0;
+    __CIOBUF__.parms[0] = CIO_READ;
+    __CIOBUF__.parms[1] = fd;
+    __CIOBUF__.parms[2] = fd >> 8;
+    __CIOBUF__.parms[3] = len;
+    __CIOBUF__.parms[4] = len >> 8;
+
+    _libgloss_cio_hook ();
+
+    out_length = __CIOBUF__.length[0] + (__CIOBUF__.length[1] & 256);
+    if (out_length > 0)
+    {
+        /* @todo The __CIOBUF__TYPE__ in cio.h only defines the CIO request buffer, and not the CIO response buffer which excludes the command byte.
+         *       This is the reason for copying from the last byte of CIOBUF__.parms[] rather than the first byte of CIOBUF__.buf
+         */
+        memcpy (buf, &__CIOBUF__.parms[8], out_length);
+    }
+
+    return __CIOBUF__.parms[0] + __CIOBUF__.parms[1] * 256;
+}
+
+#include <stdio.h>
+
+int
+read (int fd, char *buf, int len)
+{
+    int rv = 0;
+    int c;
+    int i = 0;
+#if 0
+    if (fd == 2)
+        fprintf (stderr, "%.*s", buf, len);
+    else if (fd == 1)
+        printf ("%.*s", buf, len);
+#endif
+    while (len > 0)
+    {
+        int l = (len > CIO_BUF_SIZE) ? CIO_BUF_SIZE : len;
+        c = read_chunk (fd, buf + i, l);
+        if (c < 0)
+        {
+            return c;
+        }
+        rv += c;
+        len -= c;
+        i += c;
+        if (c < CIO_BUF_SIZE)
+        {
+            return rv;
+        }
+    }
+    return rv;
+}
